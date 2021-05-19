8f556ccc8e3fe3e8b08bbab3fd27e8d33ecdb10a
gn-grc <genejogarcia.gg@gmail.com>
2021-05-19 22:11:17 +0800

    initial design and dummy data of incident reports

--- incident_reporting_web_application/Controllers/IncidentReportController.cs
+++ incident_reporting_web_application/Controllers/IncidentReportController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using incident_reporting_web_application.Models.IncidentReportView;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -14,9 +15,27 @@ namespace incident_reporting_web_application.Controllers
             return View();
         }
 
+        [HttpGet]
         public ActionResult CreateReport()
         {
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult DisplayIncidentReports()
+        {
+            //Dummy data
+            List<IncidentReportList> report = new List<IncidentReportList>();
+
+            report.Add(
+                new IncidentReportList("2020-08-3C0", "Atienza v Dimailig", "Lorem ipsum", "Lorem ipsum", DateTime.Parse("08/15/2020"), "john_dimailig", DateTime.Parse("08/17/2020"), "CONCILIATION"));
+
+            report.Add(
+                new IncidentReportList("2019-02-1A1", "Lopez v Velasquez", "Lorem ipsum", "Lorem ipsum", DateTime.Parse("02/02/2019"), "john_dimailig", DateTime.Parse("02/05/2019"), "N/A"));
+            report.Add(
+                new IncidentReportList("2019-02-1D9", "Didal v Dela Cruz", "Lorem ipsum", "Lorem ipsum", DateTime.Parse("02/27/2019"), "john_dimailig", DateTime.Parse("02/28/2019"), "MEDIATION"));
+
+            return View(report);
         }
     }
 }
---
+++ incident_reporting_web_application/Models/IncidentReportView/IncidentReportList.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace incident_reporting_web_application.Models.IncidentReportView
+{
+    public class IncidentReportList
+    {
+        //<td>Case #</td>
+        //            <td> Title</td>
+        //            <td>Incident Description</td> @*use... as the button for modal*@
+        //            <td>Relief Description</td>
+        //            <td>Date of Incident</td>
+        //            <td>Processed By</td>
+        //            <td>Date Processed</td>
+        //            <td>Hearing Status</td>
+        //            <td>Actions</td>
+
+        public string CaseNo{ get; set; }
+        public string CaseTitle { get; set; }
+        public string IncidentDesc { get; set; }
+        public string ReliefDesc { get; set; }
+        public DateTime DateOfIncident { get; set; }
+        public string ProcessedBy { get; set; }
+        public DateTime DateProcessed { get; set; }
+        public string HearingStatus { get; set; }
+
+        public IncidentReportList(string caseNo, string caseTitle, string incidentDesc, string reliefDesc, DateTime dateOfIncident, string processedBy, DateTime dateProcessed, string hearingStatus)
+        {
+            CaseNo = caseNo;
+            CaseTitle = caseTitle;
+            IncidentDesc = incidentDesc;
+            ReliefDesc = reliefDesc;
+            DateOfIncident = dateOfIncident;
+            ProcessedBy = processedBy;
+            DateProcessed = dateProcessed;
+            HearingStatus = hearingStatus;
+        }
+    }
+}
