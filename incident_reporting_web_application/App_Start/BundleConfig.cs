fa5b318b77e4104134ad1d1538ac15187ca77804
gn-grc <genejogarcia.gg@gmail.com>
2021-05-19 16:59:33 +0800

    added sidebar

--- incident_reporting_web_application/App_Start/BundleConfig.cs
+++ incident_reporting_web_application/App_Start/BundleConfig.cs
@@ -14,14 +14,6 @@ namespace incident_reporting_web_application
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
-            //Create bundel for jQueryUI  
-            //js  
-            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                      "~/Scripts/jquery-ui-{version}.js"));
-            //css  
-            bundles.Add(new StyleBundle("~/Content/cssjqryUi").Include(
-                   "~/Content/jquery-ui.css"));
-
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
