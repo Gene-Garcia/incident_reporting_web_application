afd7bf96af3aef280d681bbf4b730867114b4741
gn-grc <genejogarcia.gg@gmail.com>
2021-05-24 21:00:52 +0800

    layout updates and added layout for hearings display

---
+++ incident_reporting_web_application/Controllers/HearingController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using incident_reporting_web_application.Models.Hearing;
+using System.Web.Mvc;
+
+namespace incident_reporting_web_application.Controllers
+{
+    public class HearingController : Controller
+    {
+        // GET: Hearing
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CasesForHearing(string hearingType)
+        {
+            List<ForHearing> hearings = new List<ForHearing>();
+
+            ViewBag.HearingType = hearingType;
+
+            hearings.Add(new ForHearing(
+                "2021-05-1AX",
+                "Viceral v Martin",
+                new List<string>() { "VICERAL, Jose, Mari" },
+                new List<string>() { "MARTIN, Neut, Neri", "LUO, Yuan, Ong" },
+                new List<string>() { "LUCAS, Luke, Argon" },
+                DateTime.Parse("05/25/2021")));
+
+            hearings.Add(new ForHearing(
+                "2021-05-1AX",
+                "Viceral v Martin",
+                new List<string>() { "VICERAL, Jose, Mari" },
+                new List<string>() { "MARTIN, Neut, Neri", "LUO, Yuan, Ong" },
+                new List<string>() { "LUCAS, Luke, Argon" },
+                DateTime.Parse("05/25/2021")));
+
+            hearings.Add(new ForHearing(
+                "2021-05-1AX",
+                "Viceral v Martin",
+                new List<string>() { "VICERAL, Jose, Mari" },
+                new List<string>() { "MARTIN, Neut, Neri", "LUO, Yuan, Ong" },
+                new List<string>() { "LUCAS, Luke, Argon" },
+                DateTime.Parse("05/25/2021")));
+
+            hearings.Add(new ForHearing(
+                "2021-05-1AX",
+                "Viceral v Martin",
+                new List<string>() { "VICERAL, Jose, Mari" },
+                new List<string>() { "MARTIN, Neut, Neri", "LUO, Yuan, Ong" },
+                new List<string>() { "LUCAS, Luke, Argon" },
+                DateTime.Parse("05/25/2021")));
+
+            return View(hearings);
+        }
+    }
+}
---
+++ incident_reporting_web_application/Models/Hearing/ForHearing.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace incident_reporting_web_application.Models.Hearing
+{
+    public class ForHearing
+    {
+        public ForHearing(string caseNumber, string title, List<string> complainants, List<string> respondents, List<string> witnesses, DateTime hearingDate)
+        {
+            CaseNumber = caseNumber;
+            CaseTitle = title;
+            Complainants = complainants;
+            Respondents = respondents;
+            Witnesses = witnesses;
+            HearingDate = hearingDate;
+        }
+
+        public string CaseNumber { get; set; }
+        public string CaseTitle { get; set; }
+        public List<string> Complainants { get; set; }
+        public List<string> Respondents { get; set; }
+        public List<string> Witnesses { get; set; }
+        public DateTime HearingDate { get; set; }
+
+    }
+}
