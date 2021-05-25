84dad8292189d901e66248054044c766b9fb35eb
gn-grc <genejogarcia.gg@gmail.com>
2021-05-25 14:48:02 +0800

    added layout for managing registered accounts

--- incident_reporting_web_application/Controllers/AccountController.cs
+++ incident_reporting_web_application/Controllers/AccountController.cs
@@ -34,5 +34,58 @@ namespace incident_reporting_web_application.Controllers
         {
             return View();
         }
+
+        public ActionResult ManageAccounts()
+        {
+            List<RegisteredAccount> accounts = new List<RegisteredAccount>();
+
+            //accounts.Add(new RegisteredAccount(
+            //    "martin_v_atienza",
+            //    "ATEIN, Atienza, Villa",
+            //    DateTime.Parse("12/5/2020"),
+            //    "default",
+            //    "Lupon",
+            //    "Barangay Secretary",
+            //    false));
+
+            accounts.Add(new RegisteredAccount(
+                "john_v_dimailig",
+                "DIMAILIG, John, Villa",
+                DateTime.Parse("12/28/2020"),
+                "martin_v_atienza",
+                "Lupon",
+                "Punong Barangay",
+                false));
+
+            accounts.Add(new RegisteredAccount(
+                "lukas_d_martinez",
+                "MARTINEZ, Lukas, Domingo",
+                DateTime.Parse("01/5/2021"),
+                "martin_v_atienza",
+                "Sanguniang Barangay",
+                "Sanguniang Barangay Member",
+                false));
+
+            accounts.Add(new RegisteredAccount(
+                "chris_mol_m_dela_cruz",
+                "DELA CRUZ, Chris Mol, Marasigan",
+                DateTime.Parse("01/6/2021"),
+                "john_v_dimailig",
+                "Sanguniang Barangay",
+                "Sanguniang Barangay Member",
+                true));
+
+            accounts.Add(new RegisteredAccount(
+                "lukas_d_martinez",
+                "ATIENZA, Lar Minong, Ochoa",
+                DateTime.Parse("01/7/2021"),
+                "john_v_dimailig",
+                "Sanguniang Barangay",
+                "Sanguniang Barangay Member",
+                false));
+
+
+            return View(accounts);
+        }
     }
 }
---
+++ incident_reporting_web_application/Models/Account/RegisteredAccount.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace incident_reporting_web_application.Models.Account
+{
+    public class RegisteredAccount
+    {
+        public RegisteredAccount(string username, string fullName, DateTime registeredOn, string registeredBy, string userType, string personnelPosition, bool disabled)
+        {
+            Username = username;
+            FullName = fullName;
+            RegisteredOn = registeredOn;
+            RegisteredBy = registeredBy;
+            UserType = userType;
+            PersonnelPosition = personnelPosition;
+            Disabled = disabled;
+        }
+
+        public string Username { get; set; }
+        public string FullName { get; set; }
+        public DateTime RegisteredOn { get; set; }
+        public string RegisteredBy { get; set; }
+        public string UserType { get; set; }
+        public string PersonnelPosition { get; set; }
+        public bool Disabled { get; set; }
+    }
+}
