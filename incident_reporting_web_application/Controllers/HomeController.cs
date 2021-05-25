9371c987364e81105831e29c95f6f940b94fae2e
gn-grc <genejogarcia.gg@gmail.com>
2021-05-26 00:03:17 +0800

    added image in sidebar

--- incident_reporting_web_application/Controllers/HomeController.cs
+++ incident_reporting_web_application/Controllers/HomeController.cs
@@ -12,19 +12,5 @@ namespace incident_reporting_web_application.Controllers
         {
             return View();
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
