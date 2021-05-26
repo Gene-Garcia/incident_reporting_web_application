37e50d19b376219fb15b98ddc7a4aae65c7b9ddd
gn-grc <genejogarcia.gg@gmail.com>
2021-05-26 23:27:32 +0800

    added layout for agreement for arbitration

--- incident_reporting_web_application/Controllers/HearingController.cs
+++ incident_reporting_web_application/Controllers/HearingController.cs
@@ -78,6 +78,22 @@ namespace incident_reporting_web_application.Controllers
             return View(h);
         }
 
+        public ActionResult CreateArbitrationAgreement()
+        {
+            Hearing h = new Hearing(
+                "2021-10",
+                "Velasquez v Cortez",
+                "MEDIATION",
+                new List<string>() { "VELASQUEZ, Martin, Joe", "VELASQUEZ, Cha, Atienza" },
+                new List<string>() { "CORTEZ, Luca, Lou" },
+                new List<string>() { "MARIA, Jeremy, Roxas" },
+                DateTime.Parse("05/30/2021"),
+                DateTime.Parse("5/25/2021"),
+                DateTime.Parse("5/26/2021"));
+
+            return View(h);
+        }
+
         public ActionResult FileCourtAction()
         {
             CourtAction courtAction = new CourtAction(
