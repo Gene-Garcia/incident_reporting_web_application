c36a4c2fca2afdb33a582cec629671d55978f30a
gn-grc <genejogarcia.gg@gmail.com>
2021-05-26 23:57:38 +0800

    improvements

--- incident_reporting_web_application/Controllers/HearingController.cs
+++ incident_reporting_web_application/Controllers/HearingController.cs
@@ -22,38 +22,38 @@ namespace incident_reporting_web_application.Controllers
             ViewBag.HearingType = hearingType;
 
             hearings.Add(new ForHearing(
-                "2021-05-1AX",
-                "Viceral v Martin",
+                "2021-28",
+                "Jose Viceral vs. August Martin",
                 new List<string>() { "VICERAL, Jose, Mari" },
-                new List<string>() { "MARTIN, Neut, Neri", "LUO, Yuan, Ong" },
+                new List<string>() { "MARTIN, August, Neri", "LUO, Yuan, Ong" },
                 new List<string>() { "LUCAS, Luke, Argon" },
                 DateTime.Parse("05/25/2021"),
                 false));
 
             hearings.Add(new ForHearing(
-                "2021-05-1AX",
-                "Viceral v Martin",
-                new List<string>() { "VICERAL, Jose, Mari" },
-                new List<string>() { "MARTIN, Neut, Neri", "LUO, Yuan, Ong" },
-                new List<string>() { "LUCAS, Luke, Argon" },
+                "2021-29",
+                "Grey Santiago vs. Bodhi Tapia",
+                new List<string>() { "SANTIAGO, Grey, Asher" },
+                new List<string>() { "TAPIA, Bodhi, Lopez", "SY, Eloise, Garcia" },
+                new List<string>() ,
                 DateTime.Parse("05/25/2021"),
                 true));
 
             hearings.Add(new ForHearing(
-                "2021-05-1AX",
-                "Viceral v Martin",
-                new List<string>() { "VICERAL, Jose, Mari" },
-                new List<string>() { "MARTIN, Neut, Neri", "LUO, Yuan, Ong" },
-                new List<string>() { "LUCAS, Luke, Argon" },
+                "2021-31",
+                "Finn Dimailig vs. Jane Martes",
+                new List<string>() { "DIMAILIG, Finn, Grace" },
+                new List<string>() { "MARTES, Jane, Daisy"},
+                new List<string>() { "DUJUA, Florence, Villa" },
                 DateTime.Parse("05/25/2021"),
                 true));
 
             hearings.Add(new ForHearing(
-                "2021-05-1AX",
-                "Viceral v Martin",
-                new List<string>() { "VICERAL, Jose, Mari" },
-                new List<string>() { "MARTIN, Neut, Neri", "LUO, Yuan, Ong" },
-                new List<string>() { "LUCAS, Luke, Argon" },
+                "2021-033",
+                "Amara Paor vs. Martin Javier",
+                new List<string>() { "PAOR, Amara, Ava", "CONOL, Beck, Atienza" },
+                new List<string>() { "JAVIER, Martin, Maverick", "HUDSON, Wanda, Ong", "CHOY, Martin, Ong" },
+                new List<string>() { "REPRE, Yanna, Del Pilar", "MANALA, Eph, Talag" },
                 DateTime.Parse("05/25/2021"),
                 false));
 
@@ -65,12 +65,12 @@ namespace incident_reporting_web_application.Controllers
             if (hearingtype == "") hearingtype = "MEDIATION";
 
             Hearing h = new Hearing(
-                "2021-05-B12",
-                "Velasquez v Cortez",
+                "2021-29",
+                "Owen Velasquez vs. Oliver Cortez",
                 hearingtype.ToUpper(),
-                new List<string>(){ "VELASQUEZ, Martin, Joe", "VELASQUEZ, Cha, Atienza" },
+                new List<string>(){ "VELASQUEZ, Owen, Joe", "VELASQUEZ, Oliver, Atienza" },
                 new List<string>(){ "CORTEZ, Luca, Lou" },
-                new List<string>(){ "MARIA, Jeremy, Roxas" },
+                new List<string>(){ "MARIA, Ryker, Roxas" },
                 DateTime.Parse("05/30/2021"),
                 DateTime.Parse("5/25/2021"),
                 DateTime.Parse("5/26/2021"));
@@ -81,12 +81,12 @@ namespace incident_reporting_web_application.Controllers
         public ActionResult CreateArbitrationAgreement()
         {
             Hearing h = new Hearing(
-                "2021-10",
-                "Velasquez v Cortez",
+                "2021-29",
+                "Owen Velasquez vs. Oliver Cortez",
                 "MEDIATION",
-                new List<string>() { "VELASQUEZ, Martin, Joe", "VELASQUEZ, Cha, Atienza" },
+                new List<string>() { "VELASQUEZ, Owen, Joe", "VELASQUEZ, Oliver, Atienza" },
                 new List<string>() { "CORTEZ, Luca, Lou" },
-                new List<string>() { "MARIA, Jeremy, Roxas" },
+                new List<string>() { "MARIA, Ryker, Roxas" },
                 DateTime.Parse("05/30/2021"),
                 DateTime.Parse("5/25/2021"),
                 DateTime.Parse("5/26/2021"));
@@ -97,11 +97,11 @@ namespace incident_reporting_web_application.Controllers
         public ActionResult FileCourtAction()
         {
             CourtAction courtAction = new CourtAction(
-                "2021-05-B12",
-                "Velasquez v Cortez",
-                new List<string>() { "VELASQUEZ, Martin, Joe", "VELASQUEZ, Cha, Atienza" },
+                "2021-29",
+                "Owen Velasquez vs. Oliver Cortez",
+                new List<string>() { "VELASQUEZ, Owen, Joe", "VELASQUEZ, Oliver, Atienza" },
                 new List<string>() { "CORTEZ, Luca, Lou" },
-                new List<string>() { "MARIA, Jeremy, Roxas" },
+                new List<string>() { "MARIA, Ryker, Roxas" },
                 DateTime.Parse("05/30/2021"),
                 "MEDIATION");
             return View(courtAction);
--- incident_reporting_web_application/Controllers/NoticeController.cs
+++ incident_reporting_web_application/Controllers/NoticeController.cs
@@ -20,10 +20,10 @@ namespace incident_reporting_web_application.Controllers
         {
             List<ForNotice> data = new List<ForNotice>();
 
-            data.Add(new ForNotice("2021-01-1AZ", "Viceral v Mariano", DateTime.Parse("01/15/2021"), "marlcruz", DateTime.Parse("05/5/2021"), new List<string>() { "Summon" }));
-            data.Add(new ForNotice("2021-01-9C2", "Ocampo v Rivas", DateTime.Parse("01/17/2021"), "johndimailig", DateTime.Parse("05/6/2021"), new List<string>()));
-            data.Add(new ForNotice("2021-02-8A5", "Gomez v Hilario", DateTime.Parse("02/07/2021"), "marlcruz", DateTime.Parse("05/7/2021"), new List<string>() { "Hearing" }));
-            data.Add(new ForNotice("2021-03-0W3", "Reyes v Pineda", DateTime.Parse("03/20/2021"), "marlcruz", null, new List<string>() { "Hearing" }));
+            data.Add(new ForNotice("2021-08", "Olive Viceral vs. Eli Mariano", DateTime.Parse("01/15/2021"), "marlcruz", DateTime.Parse("05/5/2021"), new List<string>() { "Summon" }));
+            data.Add(new ForNotice("2021-09", "Della Ocampo vs. Scarlet Rivas", DateTime.Parse("01/17/2021"), "johndimailig", DateTime.Parse("05/6/2021"), new List<string>()));
+            data.Add(new ForNotice("2021-10", "Ephraim Gomez vs. Elijah Hilario", DateTime.Parse("02/07/2021"), "marlcruz", DateTime.Parse("05/7/2021"), new List<string>() { "Hearing" }));
+            data.Add(new ForNotice("2021-11", "Harold Reyes vs. Emma Pineda", DateTime.Parse("03/20/2021"), "marlcruz", null, new List<string>() { "Hearing" }));
 
             return View(data);
         }
@@ -33,13 +33,13 @@ namespace incident_reporting_web_application.Controllers
         {
             List<(DateTime, string)> schedules = new List<(DateTime, string)>();
 
-            schedules.Add( (DateTime.Parse("05/15/2021"), "Mariano v Del Pilar") );
-            schedules.Add((DateTime.Parse("05/16/2021"), "Lopez v Gurion"));
-            schedules.Add((DateTime.Parse("05/18/2021"), "Santiago v Recio"));
-            schedules.Add((DateTime.Parse("05/20/2021"), "Padilla v Baretto"));
+            schedules.Add( (DateTime.Parse("05/15/2021"), "Jasper Mariano vs. Julian Del Pilar") );
+            schedules.Add((DateTime.Parse("05/16/2021"), "Kai Lopez vs. Lucas Gurion"));
+            schedules.Add((DateTime.Parse("05/18/2021"), "Harry Santiago vs. Hazel Recio"));
+            schedules.Add((DateTime.Parse("05/20/2021"), "Max Padilla vs. Luke Baretto"));
 
             return View(new ScheduleSelection(
-                "2021-03-0W3", "Reyes v Pineda", 
+                "2021-21", "Mark Atienza vs. Lesley Dimailig", 
                 new List<string>() { "Mark Atienza", "Arch Gomez" },
                 new List<string>() { "Lesley Dimailig", "Ray Cruz", "Martin Black" },
                 schedules));
@@ -58,46 +58,46 @@ namespace incident_reporting_web_application.Controllers
             List<IncidentReport> reports = new List<IncidentReport>();
 
             reports.Add(new IncidentReport(
-                "2021-04-AC2",
-                "Hilario v Viceral",
+                "2021-16",
+                "Jude Hilario vs. Alex Viceral",
                 "MEDIATION",
                 DateTime.Parse("04/15/2021"),
                 new List<ReportNotice>() { 
                     new ReportNotice("Hearing Notice", new List<string>() { "HILARIO, Jude, Martin", "HILARIO, Judemar, Martin" }, "TO NOTIFY"),
                     new ReportNotice("Summon Notice", new List<string>() { "VICERAL, Alex, Jun" }, "TO NOTIFY"),
-                    new ReportNotice("Subpoena Notice", new List<string>() { "Orea, Milan, Mil" }, "TO NOTIFY")
+                    new ReportNotice("Subpoena Notice", new List<string>() { "GOMEZ, Ezra, Mil" }, "TO NOTIFY")
                 }));
 
             reports.Add(new IncidentReport(
-                "2021-04-4Z0",
-                "Perez v Padilla",
+                "2021-18",
+                "Nico Perez vs. Annie Padilla",
                 "MEDIATION",
                 DateTime.Parse("04/16/2021"),
                 new List<ReportNotice>() {
                     new ReportNotice("Hearing Notice", new List<string>() { "PEREZ, Nico, Martin" }, "NOTIFIED", DateTime.Parse("04/14/2021")),
-                    new ReportNotice("Summon Notice", new List<string>() { "PADILLA, Annie, Jean", "DIMAILIG, Birth, Castro" }, "TO NOTIFY")
+                    new ReportNotice("Summon Notice", new List<string>() { "PADILLA, Annie, Jean", "DIMAILIG, Journey, Castro" }, "TO NOTIFY")
                 }));
 
             reports.Add(new IncidentReport(
-                "2021-04-Z52",
-                "Cortez v Atienza",
+                "2021-19",
+                "Max Cortez vs. Joi Atienza",
                 "ARBITRATION",
                 DateTime.Parse("04/17/2021"),
                 new List<ReportNotice>() {
-                    new ReportNotice("Hearing Notice", new List<string>() { "CORTEZ, Mik, Pilar" }, "TO NOTIFY"),
+                    new ReportNotice("Hearing Notice", new List<string>() { "CORTEZ, Max, Pilar" }, "TO NOTIFY"),
                     new ReportNotice("Summon Notice", new List<string>() { "ATIENZA, Joi, Marin", "BING, Arch, Monti" }, "TO NOTIFY"),
                     new ReportNotice("Subpoena Notice", new List<string>() { "GALVEZ, Leo, Coring", "MARIA, Vhong, Viceral" }, "NOTIFIED", DateTime.Parse("04/16/2021"))
                 }));
 
             reports.Add(new IncidentReport(
-                "2021-04-AC2",
-                "Hilario v Viceral",
+                "2021-22",
+                "Jude Hilario vs. Alex Viceral",
                 "CONCILIATION",
                 DateTime.Parse("04/20/2021"),
                 new List<ReportNotice>() {
                     new ReportNotice("Hearing Notice", new List<string>() { "HILARIO, Jude, Martin", }, "TO NOTIFY"),
                     new ReportNotice("Summon Notice", new List<string>() { "VICERAL, Alex, Jun", }, "NOTIFIED", DateTime.Parse("04/18/2021")),
-                    new ReportNotice("Subpoena Notice", new List<string>() { "Orea, Milan, Mil", }, "TO NOTIFY")
+                    new ReportNotice("Subpoena Notice", new List<string>() { "LOPEZ, Colt, Mil", }, "TO NOTIFY")
                 }));
 
             return View(reports);
